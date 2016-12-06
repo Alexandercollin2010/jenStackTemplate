59520173a2a003121dc1555c48f2f476ae7e934c
Alexander Collin <alexandercollin2010@gmail.com>
2016-12-06 16:36:28 -0600

    Sent Object to Server

--- public/scripts/genericScript.js
+++ public/scripts/genericScript.js
@@ -3,6 +3,20 @@ console.log( 'genero sourced' );
 $( document ).ready( function(){
   console.log( 'JQ' );
 
+
+  // function getAthlete (){
+  //   var athleteName = $('#athleteName').val() ;
+  //   var eventName = $('#eventName').val();
+  //   var award = $('#award').val();
+  //
+  //   var athleteObject = {
+  //     athleteName: athleteName,
+  //     eventName: eventName,
+  //     award: award
+  //   };
+  //
+  // }
+
   // test get function
   var getData = function(){
     console.log( 'in getData' );
@@ -11,19 +25,26 @@ $( document ).ready( function(){
       url: '/testGet',
       success: function( response ){
         console.log( 'back from get call:', response );
+        $('#testGet').append(response.field0);
       },
       error: function(){
         console.log( 'error with ajax call...');
       }
-    })
+
+    });
   }; // end getData
 
   // test get function
   var postData = function(){
     console.log( 'in postData' );
+    var athleteName = $('#athleteName').val() ;
+    var eventName = $('#eventName').val();
+    var award = $('#award').val();
     // assemble object to send
     var objectToSend={
-      tester: 'testy'
+      athleteName: athleteName,
+      eventName: eventName,
+      award: award
     }; // end object to send
     $.ajax({
       type: 'POST',
@@ -35,17 +56,19 @@ $( document ).ready( function(){
       error: function(){
         console.log( 'error with ajax call...');
       }
-    })
+    });
   }; // end getData
 
   /// - buttons to test - ///
   $( '#testGetButton' ).on( 'click', function(){
     console.log( 'in testGetButton on click' );
     getData();
+
   }); // end testGetButton
   $( '#testPostButton' ).on( 'click', function(){
     console.log( 'in testPostButton on click' );
     postData();
+    
   }); // end testGetButton
 
 }); //end doc ready
